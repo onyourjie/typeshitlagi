ddbc75d184da62128a02cfede838ae178f9a1a41
onyourjie <filzahmufidah@student.ub.ac.id>
2025-10-16 11:16:00 +0700

    feat: :art: update data and lib
    utils from aceternity ui and struct project

---
+++ data/project.ts
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        id: 1,
+        projectTitle: "Minimalist Blog Website",    
+        projectDescription: "A clean, modern blog built with Next.js App Router and Tailwind CSS.",
+        projectImage: "/images/project-2.png",
+        projectPreviewLink: "https://emmanuel-blog-egbontech.vercel.app",
+        projectSourceCode: "https://github.com/emmanuel/blog-website"
+    },
+    {
+        id: 2,
+        projectTitle: "E-commerce Platform",
+        projectDescription: "A fully functional e-commerce platform built with Next.js and Tailwind CSS.",
+        projectImage: "/images/project-1.png",
+        projectPreviewLink: "https://emmanuel-ecommerce.vercel.app",
+        projectSourceCode: "https://github.com/emmanuel/ecommerce"
+    },
+    {
+        id: 3,
+        projectTitle: "Portfolio Website",
+        projectDescription: "A personal portfolio website to showcase projects and skills, built with Next.js and Tailwind CSS.",
+        projectImage: "/images/project-3.png",
+        projectPreviewLink: "https://emmanuel-portfolio-olive.vercel.app",
+        projectSourceCode: "https://github.com/emmanuel/portfolio"
+    }
+]
